dd01509c455e574551e5636234a8391a1979246d
GANESH KARMOKAR <31159705+gkarmokar@users.noreply.github.com>
2017-09-22 22:46:05 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,33 @@
-console.log('Loaded!');
+<!doctype html>
+
+<html>
+
+    <head>
+
+        <link href="/ui/style.css" rel="stylesheet" />
+
+    </head>
+
+    <body>
+
+        <div class="center">
+
+            <img src="https://pbs.twimg.com/profile_images/911457295858466816/EbONO3TR_400x400.jpg" class="img-medium"/>
+
+        </div>
+
+        <br>
+
+        <div class="center text-big bold">
+
+            Hi! I am Ganesh.
+
+        </div>
+
+        <script type="text/javascript" src="/ui/main.js">
+
+        </script>
+
+    </body>
+
+</html>
